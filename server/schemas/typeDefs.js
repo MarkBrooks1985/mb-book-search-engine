89a622faed3c0f514f02aa403c93a40be3e98130
MarkBrooks1985 <alynnnari@gmail.com>
2022-10-28 17:30:28 +1030

    5th commit, another update

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,15 +3,19 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
   type User {
     _id: ID!
-    name: String!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
   }
 
   type Book {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
+    bookId: ID!
+    authors: String!
+    description: String
+    title: String!
+    image: String
+    link: String
   }
 
   type Auth {
